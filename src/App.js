1530cbc0edb13f35685cc11f01c1bbf514bc75a4
Daniel Kamenkovich <>
2023-01-24 11:57:53 +0200

    Add temporary routes to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Navbar from "./components/NavBar";
+import { Routes , Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p1 className="text-lime-600 text-8xl">
-      Hello world!
-     </p1>
-    </div>
+    <>
+        <Navbar/>
+        <Routes>
+            {/*<Route path="/cards" element={<Home />} />*/}
+            {/*<Route path="/deck-builder" element={<NotFound />} />*/}
+            {/*<Route path="/my-decks" element={<NotFound />} />*/}
+        </Routes>
+    </>
   );
 }
 
